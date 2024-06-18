473241a72db3d672f8e3ec45fe5b89be1e784413
Ian <iansebastiao1234@gmail.com>
2024-06-18 20:17:36 -0300

    Atualização

--- Lista1/ex14.py
+++ Lista1/ex14.py
@@ -2,7 +2,7 @@ import math
 
 # Essa eu não sabia fazer tive que pesquisar e não entendi '-'
 
-def calcular_raizes(a, b, c):
+"""def calcular_raizes(a, b, c):
     if a == 0:
         return "Não é equação do segundo grau."
     
@@ -17,6 +17,7 @@ def calcular_raizes(a, b, c):
         raiz1 = (-b + math.sqrt(delta)) / (2*a)
         raiz2 = (-b - math.sqrt(delta)) / (2*a)
         return f"As raízes da equação são: {raiz1} e {raiz2}"
+"""
 
 # Ler os valores de a, b e c do usuário
 a = int(input("Digite o valor de a: "))
@@ -24,5 +25,16 @@ b = int(input("Digite o valor de b: "))
 c = int(input("Digite o valor de c: "))
 
 # Calcular e exibir o resultado
-resultado = calcular_raizes(a, b, c)
-print(resultado)
+delta = b**2 - 4*a*c
+
+if a == 0:
+    print("Não é equação do segundo grau.")
+elif delta <0:
+    print("Não há raizes.")
+elif delta == 0:
+    raiz = -b / (2*a)
+    print("A equação tem uma raiz:", raiz)
+else:
+    raiz1 = (-b + math.sqrt(delta)) / (2*a)
+    raiz2 = (-b - math.sqrt(delta)) / (2*a)
+    print("As raízes da esquação são:", raiz1, raiz2)
